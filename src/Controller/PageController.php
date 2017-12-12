fde56ba50270eec3c99c6a1c1f123decb5fc5f20
GolovaVasilenko <vasilenko75@gmail.com>
2017-12-12 08:15:21 +0200

    add first template

---
+++ src/Controller/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        // replace this line with your own code!
+        return $this->render('page/home.html.twig', [
+        	'title' => 'home page',
+	        'body' => 'some text or description',
+        ]);
+    }
+}
